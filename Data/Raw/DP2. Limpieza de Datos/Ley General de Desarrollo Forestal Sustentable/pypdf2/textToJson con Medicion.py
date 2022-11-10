6d4164dadf721758a26cf3b1d748337ace4d6692
YesseniaD <108906954+YesseniaD@users.noreply.github.com>
2022-11-09 23:43:38 -0600

    Add files via upload

---
+++ Data/Raw/DP2. Limpieza de Datos/Ley General de Desarrollo Forestal Sustentable/pypdf2/textToJson con Medicion.py
@@ -0,0 +1,38 @@
+﻿import json
+import timeit
+import psutil
+
+start = timeit.default_timer()
+filename = 'Ley General de Desarrollo Forestal Sustentable.txt'
+  
+my_list = []
+
+dict1 = {}
+  
+with open(filename) as fh:
+  
+    for line in fh:
+  
+        description = line.split()
+  
+        my_list.append(description)
+        
+
+
+def flatten(A):
+    rt = []
+    for i in A:
+        if isinstance(i,list): rt.extend(flatten(i))
+        else: rt.append(i)
+    return rt
+
+list_array = flatten(my_list)
+
+out_file = open("Ley General de Desarrollo Forestal Sustentable.json", "w")
+json.dump(list_array, out_file)
+out_file.close()
+
+end = timeit.default_timer()
+print('Tiempo total de ejecucion: ', end - start)
+print('Memoria RAM del Proceso: ', psutil.virtual_memory()[2])
+print('Memoria RAM TOTAL: ', psutil.virtual_memory().total)
