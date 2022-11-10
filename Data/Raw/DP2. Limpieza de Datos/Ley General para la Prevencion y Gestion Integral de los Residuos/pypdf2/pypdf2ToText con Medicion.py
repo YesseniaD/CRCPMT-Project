84bdd07aa3e882800b564df2c97e7749d464b1a3
YesseniaD <108906954+YesseniaD@users.noreply.github.com>
2022-11-09 23:45:03 -0600

    Add files via upload

---
+++ Data/Raw/DP2. Limpieza de Datos/Ley General para la Prevencion y Gestion Integral de los Residuos/pypdf2/pypdf2ToText con Medicion.py
@@ -0,0 +1,41 @@
+﻿from PyPDF2 import PdfFileReader, PdfFileWriter
+import timeit
+import psutil
+    
+start = timeit.default_timer()
+file_path = '.\Ley General para la Prevencion y Gestion Integral de los Residuos.pdf'
+pdf = PdfFileReader(file_path)
+
+with open('Ley General para la Prevencion y Gestion Integral de los Residuos.txt', 'w') as f:
+    for page_num in range(pdf.numPages):
+        # print('Page: {0}'.format(page_num))
+        pageObj = pdf.getPage(page_num)
+        
+        try:
+            txt = pageObj.extractText()
+            print(''.center(100, '-'))
+        except:
+            pass
+        else:
+            f.write('Page {0}\n'.format(page_num+1))
+            f.write(''.center(100, '-'))
+            f.write(txt)
+    f.close()
+    
+filename = "Ley General para la Prevencion y Gestion Integral de los Residuos.txt"
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "El archivo " + filename + " no existe"
+    print(msg)
+else:
+    words = contents.split()
+    num_words = len(words)
+    print("El archivo " + filename + " contiene: " + str(num_words) + " palabras")
+
+
+end = timeit.default_timer()
+print('Tiempo total de ejecucion: ', end - start)
+print('Memoria RAM del Proceso: ', psutil.virtual_memory()[2])
+print('Memoria RAM TOTAL: ', psutil.virtual_memory().total)
