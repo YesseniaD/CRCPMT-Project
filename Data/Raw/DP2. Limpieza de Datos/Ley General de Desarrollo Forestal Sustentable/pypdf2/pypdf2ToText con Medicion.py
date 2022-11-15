c9e83d9fabccb0ab77ed818e35e1c8c5a581eb41
YesseniaD <108906954+YesseniaD@users.noreply.github.com>
2022-11-15 08:12:14 -0600

    Add files via upload

---
+++ Data/Raw/DP2. Limpieza de Datos/Ley General de Desarrollo Forestal Sustentable/pypdf2/pypdf2ToText con Medicion.py
@@ -0,0 +1,55 @@
+﻿from PyPDF2 import PdfFileReader, PdfFileWriter
+import timeit
+import psutil
+    
+start = timeit.default_timer()
+start1 = timeit.default_timer()
+file_path = '.\Ley General de Desarrollo Forestal Sustentable.pdf'
+pdf = PdfFileReader(file_path)
+
+with open('Ley General de Desarrollo Forestal Sustentable.txt', 'w') as f:
+    for page_num in range(pdf.numPages):
+        # print('Page: {0}'.format(page_num))
+        pageObj = pdf.getPage(page_num)
+        
+        try:
+            txt = pageObj.extractText()
+            print(''.center(100, '-'))
+        except:
+            pass
+        else:
+            f.write('Page {0}\n'.format(page_num+1))
+            f.write(''.center(100, '-'))
+            f.write(txt)
+    f.close()
+end1 = timeit.default_timer()
+    
+start2 = timeit.default_timer()
+filename = "Ley General de Desarrollo Forestal Sustentable.txt"
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "El archivo " + filename + " no existe"
+    print(msg)
+else:
+    words = contents.split()
+    num_words = len(words)
+
+end2 = timeit.default_timer()
+end = timeit.default_timer()
+
+print("El archivo " + filename + " contiene: " + str(num_words) + " palabras")
+
+# print('Tiempo 1: ', start1)
+# print('Tiempo END 1: ', end1)
+# print('Tiempo 2: ', start2)
+# print('Tiempo END 2: ', end2)
+t1=end1 - start1
+t2=end2 - start2
+print('Tiempo de pdf a txt: ', t1)
+print('Tiempo de contador de palabras: ', t2)
+print('Tiempo total', t1+t2)
+print('Tiempo total de ejecucion: ', end - start)
+print('Memoria RAM del Proceso: ', psutil.virtual_memory()[2])
+print('Memoria RAM TOTAL: ', psutil.virtual_memory().total)
